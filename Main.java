1c1140455decce00bd38d0b7c0c78799cc605169
nicolezhangg <43891915+nicolezhangg@users.noreply.github.com>
2018-10-24 11:22:27 -0700

    up to task 10

--- Main.java
+++ Main.java
@@ -249,11 +249,7 @@ class Main
            for (int i=0 ; i<k ; i++)
                System.out.println(ar4[i]);
         
-        
-        
-        
-        
-        
+              
         
         /*
          * Task 10.  Shift the elements of ar4 right by 1
@@ -262,7 +258,22 @@ class Main
          * new   ar4  9 1 3 5 7
          */
         
+         System.out.println("** Task 10 ** ");
+         System.out.println("Original ar4");
+         for (int i=0 ; i<ar4.length ; i++)
+             System.out.println(ar4[i]);
         
+
+         int temp10=ar4[ar4.length-1];
+         
+         for (int i=ar4.length-1 ; i>0 ; i--)
+            ar4[i]=ar4[i-1];
+         
+         ar4[0]=temp10;
+
+         System.out.println("New ar4");
+         for (int i=0 ; i<ar4.length ; i++)
+             System.out.println(ar4[i]);
         /*
          * Task 11.  Reverse the order of elements in ar2
          */
